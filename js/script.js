3a03659638b1ccad9cafe709f824ce129d9145a0
박나영 <89637830+xmqn@users.noreply.github.com>
2021-11-24 00:46:33 +0900

    21.11.24 리디자인파트
    21.11.24 리디자인파트

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-window.load = function(){};
-$(document).ready(function(){
+window.load = function () {};
+$(document).ready(function () {
     new Swiper('.sw-publ', {
         slidesPerView: 4,
         slidesPerGroup: 4,
@@ -7,6 +7,15 @@ $(document).ready(function(){
         navigation: {
             nextEl: ".sw-publ-next",
             prevEl: ".sw-publ-prev",
+        },
+    });
+    new Swiper('.sw-rede', {
+        slidesPerView: 1,
+        slidesPerGroup: 2,
+        spaceBetween: 0,
+        pagination: {
+            el: ".sw-rede-pg",
+            clickable: true,
         },
     });
 });
