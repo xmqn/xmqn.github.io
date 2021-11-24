a1f710d0f7ade2a2237cc992964687743af0ae61
박나영 <89637830+xmqn@users.noreply.github.com>
2021-11-24 12:11:37 +0900

    21.11.24+
    스크롤바
    퍼블리싱파트 호버
    리디자인파트 움직임
    완료

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 window.load = function () {};
 $(document).ready(function () {
+        
+    $("body").niceScroll({
+        zindex: 9999,
+        cursoropacitymin: 0,
+        cursoropacitymax: 1,
+        cursorcolor: "#0030ff",
+        cursorwidth: "1rem",
+        scrollspeed: 80,
+        hidecursordelay: 3000
+    });
+
     new Swiper('.sw-publ', {
         slidesPerView: 4,
-        slidesPerGroup: 4,
         spaceBetween: 0,
         navigation: {
             nextEl: ".sw-publ-next",
             prevEl: ".sw-publ-prev",
         },
+        loop: true
     });
     new Swiper('.sw-rede', {
         slidesPerView: 1,
-        slidesPerGroup: 2,
         spaceBetween: 0,
+        navigation: {
+            nextEl: ".sw-rede-next",
+            prevEl: ".sw-rede-prev",
+        },
         pagination: {
             el: ".sw-rede-pg",
             clickable: true,
         },
+        loop: true
     });
 });
